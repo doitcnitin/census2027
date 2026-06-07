af2be2e93708b59ea344a385dded54d902dc9b11
doitcnitin <samparktonitin01@gmail.com>
2026-06-07 13:54:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,177 +1,144 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbw5O5MyRFpp8HOPlr4Yw95LjMXB-1PEBPGmYpwjRcKONg69LuOPCI8uY_3V-YQ8-A_-Yg/exec";
 
-window.onload = function(){
+document.addEventListener(
+"DOMContentLoaded",
+function(){
+
+  loadCircles();
+
+  document
+  .getElementById("censusPost")
+  .addEventListener(
+    "change",
+    refreshHLBDropdown
+  );
+
+  document
+  .getElementById("circleNo")
+  .addEventListener(
+    "change",
+    onCircleChange
+  );
+
+  document
+  .getElementById("hlbNo")
+  .addEventListener(
+    "change",
+    onHLBChange
+  );
+
+});
+
+function loadCircles(){
 
   fetch(
     API_URL +
     "?action=circles"
   )
-  .then(response => response.json())
-  .then(loadCircles)
-  .catch(err=>{
 
-    alert("Error Loading Circles");
+  .then(r=>r.json())
 
-    console.error(err);
+  .then(function(circles){
 
-  });
+    const dropdown =
+    document.getElementById(
+      "circleNo"
+    );
 
-};
+    dropdown.innerHTML =
+    '<option value="">Select</option>';
 
-function loadCircles(circles){
+    circles.forEach(circle=>{
 
-  const dropdown =
-  document.getElementById(
-    "circleNo"
-  );
+      dropdown.innerHTML +=
+      `<option value="${circle}">
+      ${circle}
+      </option>`;
 
-  dropdown.innerHTML =
-  '<option value="">Select</option>';
-
-  circles.forEach(circle=>{
-
-    dropdown.innerHTML +=
-    `
-    <option value="${circle}">
-    ${circle}
-    </option>
-    `;
+    });
 
   });
 
 }
 
-function loadHLBs(hlbs){
-
-  const dropdown =
-  document.getElementById(
-    "hlbNo"
-  );
-
-  dropdown.innerHTML =
-  '<option value="">Select</option>';
-
-  hlbs.forEach(hlb=>{
-
-    dropdown.innerHTML +=
-    `
-    <option value="${hlb}">
-    ${hlb}
-    </option>
-    `;
-
-  });
-
-}
-
-document
-.getElementById("censusPost")
-.addEventListener(
-"change",
-function(){
-
-  refreshHLBDropdown();
-
-}
-);
-
-document
-.getElementById("circleNo")
-.addEventListener(
-"change",
-function(){
+function onCircleChange(){
 
   const circle =
   this.value;
 
   if(!circle){
-
-    document.getElementById(
-      "supervisorName"
-    ).value = '';
-
-    document.getElementById(
-      "supervisorMobile"
-    ).value = '';
-
     return;
-
   }
 
   Promise.all([
 
-  fetch(
-    API_URL +
-    "?action=supervisor" +
-    "&circleNo=" +
-    encodeURIComponent(circle)
-  ).then(r=>r.json()),
+    fetch(
+      API_URL +
+      "?action=supervisor" +
+      "&circleNo=" +
+      encodeURIComponent(circle)
+    ).then(r=>r.json()),
 
-  fetch(
-    API_URL +
-    "?action=hlbs" +
-    "&circleNo=" +
-    encodeURIComponent(circle)
-  ).then(r=>r.json())
+    fetch(
+      API_URL +
+      "?action=hlbs" +
+      "&circleNo=" +
+      encodeURIComponent(circle)
+    ).then(r=>r.json())
 
-])
+  ])
 
-.then(([supervisorData, hlbData])=>{
+  .then(([supData, hlbData])=>{
 
-  document.getElementById(
-    "supervisorName"
-  ).value =
-  supervisorData.supervisorName || '';
-
-  document.getElementById(
-    "supervisorMobile"
-  ).value =
-  supervisorData.supervisorMobile || '';
-
-  if(
     document.getElementById(
-      "censusPost"
-    ).value === "Enumerator"
-  ){
+      "supervisorName"
+    ).value =
+    supData.supervisorName || '';
 
-    loadHLBs(hlbData);
+    document.getElementById(
+      "supervisorMobile"
+    ).value =
+    supData.supervisorMobile || '';
 
-  }
+    if(
+      document.getElementById(
+        "censusPost"
+      ).value === "Enumerator"
+    ){
 
-})
-  .catch(err=>{
+      const hlb =
+      document.getElementById(
+        "hlbNo"
+      );
 
-    console.error(err);
+      hlb.innerHTML =
+      '<option value="">Select</option>';
 
-    alert(
-      "Unable to load supervisor details"
-    );
+      hlbData.forEach(item=>{
+
+        hlb.innerHTML +=
+        `<option value="${item}">
+        ${item}
+        </option>`;
+
+      });
+
+    }
 
   });
 
 }
-);
 
 function refreshHLBDropdown(){
 
   const post =
-  document
-  .getElementById(
+  document.getElementById(
     "censusPost"
-  )
-  .value;
-
-  const circle =
-  document
-  .getElementById(
-    "circleNo"
-  )
-  .value;
+  ).value;
 
   const hlb =
-  document
-  .getElementById(
+  document.getElementById(
     "hlbNo"
   );
 
@@ -182,89 +149,28 @@ function refreshHLBDropdown(){
     hlb.innerHTML =
     '<option value="">Not Applicable</option>';
 
-    document
-    .getElementById(
-      "enumeratorName"
-    )
-    .value = '';
-
-    document
-    .getElementById(
-      "mobileNumber"
-    )
-    .value = '';
-
     return;
 
   }
 
   hlb.disabled = false;
 
-  if(!circle){
-
-    hlb.innerHTML =
-    '<option value="">Select Circle First</option>';
-
-    return;
-
-  }
-
-  fetch(
-
-    API_URL +
-    "?action=hlbs" +
-    "&circleNo=" +
-    encodeURIComponent(circle)
-
-  )
-
-  .then(r=>r.json())
-
-  .then(loadHLBs)
-
-  .catch(err=>{
-
-    console.error(err);
-
-    alert(
-      "Unable to load HLB list"
-    );
-
-  });
-
 }
-document
-.getElementById("hlbNo")
-.addEventListener(
-"change",
-function(){
+
+function onHLBChange(){
 
   const circle =
-  document
-  .getElementById(
+  document.getElementById(
     "circleNo"
-  )
-  .value;
+  ).value;
 
   const hlb =
-  this.value;
+  document.getElementById(
+    "hlbNo"
+  ).value;
 
   if(!circle || !hlb){
-
-    document
-    .getElementById(
-      "enumeratorName"
-    )
-    .value = '';
-
-    document
-    .getElementById(
-      "mobileNumber"
-    )
-    .value = '';
-
     return;
-
   }
 
   fetch(
@@ -283,35 +189,20 @@ function(){
   .then(function(data){
 
     console.log(
-      "ENUMERATOR DATA",
+      "Enumerator:",
       data
     );
 
-    document
-    .getElementById(
+    document.getElementById(
       "enumeratorName"
-    )
-    .value =
+    ).value =
     data.enumeratorName || '';
 
-    document
-    .getElementById(
+    document.getElementById(
       "mobileNumber"
-    )
-    .value =
+    ).value =
     data.mobile || '';
-
-  })
-
-  .catch(err=>{
-
-    console.error(err);
-
-    alert(
-      "Unable to load Enumerator Details"
-    );
 
   });
 
 }
-);
