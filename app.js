895d315e58ce72819064f88bb039e83add1872a9
doitcnitin <samparktonitin01@gmail.com>
2026-06-07 12:50:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbw5O5MyRFpp8HOPlr4Yw95LjMXB-1PEBPGmYpwjRcKONg69LuOPCI8uY_3V-YQ8-A_-Yg/exec";
+
+window.onload = function(){
+
+fetch(
+API_URL +
+"?action=circles"
+)
+
+.then(response =>
+response.json()
+)
+
+.then(loadCircles)
+
+.catch(err=>{
+
+alert(
+"Error Loading Circles"
+);
+
+console.error(err);
+
+});
+
+};
+
+function loadCircles(circles){
+
+const dropdown =
+document.getElementById(
+"circleNo"
+);
+
+dropdown.innerHTML =
+'<option value="">Select</option>';
+
+circles.forEach(circle=>{
+
+dropdown.innerHTML +=
+`
+<option value="${circle}">
+${circle}
+</option>
+`;
+
+});
+
+}
